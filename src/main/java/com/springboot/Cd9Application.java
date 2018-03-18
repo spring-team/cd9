0bf1ca698115f3bbe0456e19ab91be6813b678f5
Christian Dupuis <cd@atomist.com>
2018-03-18 21:04:15 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/springboot/Cd9Application.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cd9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cd9Application.class, args);
+	}
+}
